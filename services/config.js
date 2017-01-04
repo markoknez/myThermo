08f4a09d5211b3efd97d727e2c654025e9871090
Marko Knezevic <ubuntu@webmail.mrostudios.com>
2017-01-04 09:48:03 +0000

    added gitignore for config.js

--- services/config.js
+++ services/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	mqttWeatherUsername : "",
-	mqttWeatherPassword : "",
-	mqttMongodbUsername : "",
-	mqttMongodbPassword : ""
+	mqttWeatherUsername : "service_weather",
+	mqttWeatherPassword : "123456",
+	mqttMongodbUsername : "service_mongodb",
+	mqttMongodbPassword : "123456"
 };
 
