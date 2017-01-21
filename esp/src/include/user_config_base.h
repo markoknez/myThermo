138a745df1056efacc64b698bc1ab29bb3ac8572
Marko Knezevic <markoknez@gmail.com>
2017-01-21 20:57:11 +0100

    ds18b20 crc error check implemented - not tested yet

--- esp/src/include/user_config_base.h
+++ esp/src/include/user_config_base.h
@@ -2,7 +2,15 @@
 #define __USER_CONFIG_BASE_H__
 
 #define USE_OPTIMIZE_PRINTF
-#include "user_config.wall.h"
+#include "user_config.local.h"
+
+#if defined(DEBUG_ON)
+#define INFO( format, ... ) os_printf( "%s - INFO - "format, __FILE__, ## __VA_ARGS__ )
+#define ERROR( format, ... ) os_printf( "%s - ERROR - "format, __FILE__, ## __VA_ARGS__ )
+#else
+#define INFO( format, ... )
+#define ERROR( format, ... )
+#endif
 
 #endif
 
